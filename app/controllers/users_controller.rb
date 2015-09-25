5351d74d14ff354643cefb45f5b736e0b5367172
stuckiest <stuckiest@gmail.com>
2015-09-25 15:54:00 -0600

    added crud actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
   def index
+    @user = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
 
   def create
@@ -20,12 +23,28 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :age, :email, :gender, :looking_for, :city, :state)
   end
 
+  def find_user
+    @user = User.find(params[:id])
+  end 
 
 end
 
--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,3 +1,53 @@
 class Crime < ActiveRecord::Base
   belongs_to :user
+  before_action :find_crime, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @crime = Crime.all
+  end
+
+  def show
+  end
+
+  def new
+    @crime = Crime.new
+  end
+
+  def create
+    crime = Crime.new(crime_params)
+    if crime.save
+      redirect_to crimes_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @crime.update(crime_params)
+      redirect_to crimes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @crime.destroy
+    redirect_to crimes_path
+  end
+
+  private
+
+  def interest_params
+    params.require(:crime).permit(:crime, :sentence)
+  end
+
+  def find_user
+    @crime = Crime.find(params[:id])
+  end 
+
+end
+
 end
