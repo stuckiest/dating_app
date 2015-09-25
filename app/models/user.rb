c339c70570beaa0f4484ca1803f101c0f6a1f185
stuckiest <stuckiest@gmail.com>
2015-09-25 15:28:18 -0600

    added model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+  has_many :interests, dependent: :destroy
+  validates_presence_of :name
+
+def self.males
+    where(gender: 'male')
+  end
+
+  def self.females
+    where(gender: 'female')
+  end
+
+  def self.between_ages(min_age, max_age)
+    where("age > ? AND age < ?", min_age, max_age)
+  #  this is how to call this method: Person.all.males.between_ages(18, 28)
+  end
+  
+
+
 end
